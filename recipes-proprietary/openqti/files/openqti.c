c3987cb79643fe8ce073afa10e708925ca6e4e3c
biktor <biktor_ghub@biktorgj.com>
2020-12-06 18:18:07 +0100

    Just a placeholder

---
+++ recipes-proprietary/openqti/files/openqti.c
@@ -0,0 +1,14 @@
+/* This is just a dummy */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+  int exit_code = 0;
+  printf("Hello world \n");
+
+return exit_code;
+}
