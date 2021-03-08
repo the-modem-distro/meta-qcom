d1bb1235c69ae4b6bbf19155156d9ff0e1fb6343
Biktor <biktor@biktorgj.com>
2021-03-08 07:03:28 +0100

    Beginning work on responding to stuff. Of course it doesn't work yet. At least now I know how it is roughly structured to know where to look for the commands being sent. Need more examples from the atfwd_daemon blob replies to the modem to either OK or CME ERROR them when they arrive

--- recipes-modem/openatfwd/files/atfwd.c
+++ recipes-modem/openatfwd/files/atfwd.c
@@ -70,10 +70,94 @@ struct atcmd_reg_request * make_packet(int tid, const char *command) {
   atcmd->var1 = atcmd->var2 + 3;
   return atcmd;
 }
+/*
+EXAMPLE RESPONSE
+read(3, 
+Response header:
+"\x04\x01\x00\x21\x00\x71\x00\x01\x12\x00\x01\x00\x00\x00\x01\x00\x00\x00\x09
+Command sent through the AT IF
+\x2b\x48\x45\x52\x45\x57\x45\x47\x4f
+No clue what this is but it looks like places to get params? Like when you send qurccfg="par1","par2"?
+\x10\x00\x00\x11\x56\x00
+\x10\x04\x00\x01\x00\x00\x00
+\x11\x04\x00\x00\x00\x00\x00
+\x12\x04\x00\x0d\x00\x00\x00
+\x13\x04\x00\x0a\x00\x00\x00
+\x14\x04\x00\x00\x00\x00\x00
+\x15\x04\x00\x00\x00\x00\x00
+\x16\x04\x00\x01\x00\x00\x00
+\x17\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00
+\x18\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00
+\x19\x04\x00\x00\x00\x00\x00", 8192) = 120
 
+*/
 int handle_response(struct qmi_device *qmidev, char *buf, unsigned int sz) {
+  int j, sckret;
+  int packet_size;
+  uint8_t cmdsize, cmdend;
+  char *parsedcmd;
+  int cmd_id = -1;
+  struct at_command_simple_reply *cmdreply;
+  cmdreply = calloc(1, sizeof(struct at_command_simple_reply));
+
   fprintf(stdout, "Handle response: not implemented \n");
-  printf("%s\n", buf);
+  if (sz < 16) {
+    fprintf(stderr, "Packet too small\n");
+    return 0;
+  }
+  if (buf[0] != 0x04) {
+    fprintf(stderr, "Unknown control ID!\n");
+    return 0;
+  }
+  if (buf[3] != 0x21) {
+    fprintf(stderr, "Unknown message type!\n");
+    return 0;
+  }
+
+  packet_size = buf[5];
+  fprintf(stdout, "Packet size is %i, received total size is %i\n", packet_size, sz);
+
+  cmdsize = buf[18];
+  cmdend = 18 + cmdsize;
+
+  parsedcmd = calloc(cmdsize +1, sizeof(char));
+  strncpy(parsedcmd, buf+19, cmdsize);
+
+  fprintf(stdout, "Command is %s\n", parsedcmd);
+  for (j = 0; j < (sizeof(at_commands) / sizeof(at_commands[0])); j++) {
+    if (strcmp(parsedcmd, at_commands[j].cmd) == 0) {
+      fprintf(stdout, "Command match: %s, ID %i\n", at_commands[j].cmd, at_commands[j].command_id);
+      cmd_id = at_commands[j].command_id;
+    }
+  }
+
+  cmdreply->ctlid = 0x04;
+  cmdreply->transaction_id = htole16(qmidev->transaction_id);
+  cmdreply->msgid = htole16(33);
+  cmdreply->length = sizeof(struct at_command_simple_reply)- (3*sizeof(uint16_t)) - sizeof(uint8_t);
+  cmdreply->command = 0;
+  cmdreply->result = 1;
+  cmdreply->response = 3;
+  /*
+  Not expecting this to work, just finding a way of not needing
+  to reboot everytime I send a command for now. I'll worry about
+  correctness later */
+
+  if (cmd_id == -1) { // Send an error
+    fprintf(stderr, "Puking error as success :)\n");
+    sckret = sendto(qmidev -> fd, cmdreply, sizeof(cmdreply), MSG_DONTWAIT, (void * ) & qmidev -> socket, sizeof(qmidev -> socket));
+
+  } else {
+    fprintf(stderr, "Puking success as success\n");
+    sckret = sendto(qmidev -> fd, cmdreply, sizeof(cmdreply), MSG_DONTWAIT, (void * ) & qmidev -> socket, sizeof(qmidev -> socket));
+
+    switch (cmd_id) {
+      default: // Not implemented, send an OK to everything for now
+        break;
+    }
+  }
+  fprintf(stdout, "socket send res: %i\n", sckret);
+  qmidev->transaction_id++;
   return 0;
 }
 
@@ -81,7 +165,6 @@ int main(int argc, char ** argv) {
   printf("Let's play!\n");
   int ret,pret;
   fd_set readfds;
-  int tid = 1; // Transaction ID, to keep track of the commands sent
   int sckret;
   int i,j;
   int max_fd = 100; // max ID of FDs to track in select()
@@ -99,8 +182,8 @@ int main(int argc, char ** argv) {
 
   for (j = 0; j < (sizeof(at_commands) / sizeof(at_commands[0])); j++) {
     printf("Pushing command %i: %s \n", at_commands[j].command_id, at_commands[j].cmd);
-    atcmd = make_packet(tid, at_commands[j].cmd);
-    tid++;
+    atcmd = make_packet(qmidev->transaction_id, at_commands[j].cmd);
+    qmidev->transaction_id++;
     sckret = sendto(qmidev -> fd, atcmd, strlen(at_commands[j].cmd) + sizeof(struct atcmd_reg_request) +43, MSG_DONTWAIT, (void * ) & qmidev -> socket, sizeof(qmidev -> socket));
     if (sckret < 0) {
       fprintf(stderr, "Failed to send command %s \n", at_commands[j].cmd);
@@ -133,14 +216,15 @@ int main(int argc, char ** argv) {
 }
 
 /*
-EXAMPLE RESPONSE
+EXAMPLE RESPONSE: + HEREWEGO
 read(3, 
 Response header:
 "\x04\x01\x00\x21\x00\x71\x00\x01\x12\x00\x01\x00\x00\x00\x01\x00\x00\x00\x09
 Command sent through the AT IF
 \x2b\x48\x45\x52\x45\x57\x45\x47\x4f
 No clue what this is but it looks like places to get params? Like when you send qurccfg="par1","par2"?
-\x10\x00\x00\x11\x56\x00
+\x10\x00\x00
+\x11\x56\x00
 \x10\x04\x00\x01\x00\x00\x00
 \x11\x04\x00\x00\x00\x00\x00
 \x12\x04\x00\x0d\x00\x00\x00
@@ -151,5 +235,42 @@ No clue what this is but it looks like places to get params? Like when you send 
 \x17\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00
 \x18\x0c\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00
 \x19\x04\x00\x00\x00\x00\x00", 8192) = 120
+
+EXAMPLE RESPONSE: +QWISO                
+0x04 0x01 0x00 0x21 0x00 0x72 0x00 0x01 0x0f 0x00 0x01 0x00 0x00 0x00 0x05 0x00 0x00 0x00 0x06 
+0x2b 0x51 0x57 0x49 0x53 0x4f 
+0x10 0x04 0x00 
+0x10 0x01 0x00 0x00 
+0x11 0x56 0x00 0x10 0x04 0x00 0x01 0x00 0x00 0x00 
+0x11 0x04 0x00 0x00 0x00 0x00 0x00 
+0x12 0x04 0x00 0x0d 0x00 0x00 0x00 
+0x13 0x04 0x00 0x0a 0x00 0x00 0x00 
+0x14 0x04 0x00 0x00 0x00 0x00 0x00 
+0x15 0x04 0x00 0x00 0x00 0x00 0x00 
+0x16 0x04 0x00 0x01 0x00 0x00 0x00 
+0x17 0x0c 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 
+0x18 0x0c 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 0x00 
+0x19 0x04 0x00 0x00 0x00 0x00 0x00 
+
+
+struct at_command_modem_response {
+	// QMI Header
+	uint8_t ctlid; // 0x04
+	uint16_t transaction_id; // incremental counter for each request, why 1 in its response? separate counter?
+	uint16_t msgid; // 0x21 0x00// RESPONSE!
+	uint16_t length; // 0x71 0x00 Sizeof the entire packet
+
+	// the request packet itself
+	// Obviously unfinished :)
+	uint8_t dummy1; // always 0x00 0x01
+	uint8_t var1; // 0x12?
+	uint16_t dummy2; // always 0x00 0x01
+	uint16_t dummy3; // always 0x00 0x00
+	uint16_t dummy4; // always 0x00 0x01
+  uint16_t dummy5; // 0x00 0x00
+	uint8_t cmd_len; // 0x09?
+  char *command;
+  // The rest I have no idea yet, more sample packets are needed..
+} __attribute__((packed));
 
 */
--- recipes-modem/openatfwd/files/atfwd.h
+++ recipes-modem/openatfwd/files/atfwd.h
@@ -2,29 +2,15 @@
 #define _ATFWD_H_
 #include <stdbool.h>
 
-#define VERSION "0.0.0"
-
 /* Devices */
-#define DPL "/dev/dpl_ctrl"
-#define RMNET_CTL "/dev/rmnet_ctrl"
-#define SMD_CNTL "/dev/smdcntl8"
 #define IPC_ROUTER 27 // AF_IB
 #define IPC_ROUTER_ADDR 2 // Kernel IPC driver address
 #define IPC_ROUTER_ADDRTYPE 2 // From the decoded packets, this should be 2, not 1 like for DPM
 #define IPC_HEXAGON_NODE 0x3
 #define IPC_HEXAGON_PORT  0x1b
-#define RMNET_CONN_ID 8
 #define IPC_IOCTL_MAGIC 0xc3
 #define IOCTL_BIND_TOIPC _IOR(IPC_IOCTL_MAGIC, 4, uint32_t)
 #define MAX_PACKET_SIZE 2048 // rmnet max packet size
-
-#define QTI_IOCTL_MAGIC	'r'
-#define GET_LINE_STATE	_IOR(QTI_IOCTL_MAGIC, 2, int)
-#define EP_LOOKUP _IOR(QTI_IOCTL_MAGIC, 3, struct ep_info)
-
-
-#define QMI_SERVICE_CTL         0x00
-#define QMI_DEFAULT_BUF_SIZE    0x1000
 
 struct msm_ipc_port_addr {
 	uint32_t node_id;
@@ -49,54 +35,6 @@ struct sockaddr_msm_ipc {
 	unsigned char reserved;
 };
 
-enum peripheral_ep_type {
-	DATA_EP_TYPE_RESERVED	= 0x0,
-	DATA_EP_TYPE_HSIC	= 0x1,
-	DATA_EP_TYPE_HSUSB	= 0x2,
-	DATA_EP_TYPE_PCIE	= 0x3,
-	DATA_EP_TYPE_EMBEDDED	= 0x4,
-	DATA_EP_TYPE_BAM_DMUX	= 0x5,
-};
-
-struct peripheral_ep_info {
-	enum peripheral_ep_type ep_type;
-	unsigned long int peripheral_iface_id;
-};
-
-struct ipa_ep_pair {
-	unsigned long int cons_pipe_num;
-	unsigned long int prod_pipe_num;
-};
-
-struct ep_info {
-	struct peripheral_ep_info	ph_ep_info;
-	struct ipa_ep_pair ipa_ep_pair;
-};
-
-
-/* Playing around */
-struct portmapper_port_map_arr {
-	char *port_name;
-	struct peripheral_ep_info epinfo;
-};
-
-struct portmapper_open_request {
-	uint8_t is_valid_ctl_list;
-	uint32_t ctl_list_length;
-	struct portmapper_port_map_arr hw_port_map[2];
-
-	uint8_t is_valid_hw_list;
-	uint32_t hw_list_length;
-	struct ep_info hw_epinfo;
-
-	uint8_t is_valid_sw_list;
-	uint32_t sw_list_length;
-	struct portmapper_port_map_arr sw_port_map[2];
-};
-
-
-#define DIAG_SERVICE 4097
-typedef uint8_t ctl_state_t;
 
 static const struct {
 	unsigned int command_id;
@@ -213,78 +151,6 @@ static const struct {
 	{108, "+HEREWEGO", },
 };
 
-// From lookup.c, useful to know which service you're at
-static const struct {
-	unsigned int service;
-	unsigned int ifilter;
-	const char *name;
-} common_names[] = {
-	{ 0, 0, "Control service" },
-	{ 1, 0, "Wireless Data Service" },
-	{ 2, 0, "Device Management Service" },
-	{ 3, 0, "Network Access Service" },
-	{ 4, 0, "Quality Of Service service" },
-	{ 5, 0, "Wireless Messaging Service" },
-	{ 6, 0, "Position Determination Service" },
-	{ 7, 0, "Authentication service" },
-	{ 8, 0, "AT service" },
-	{ 9, 0, "Voice service" },
-	{ 10, 0, "Card Application Toolkit service (v2)" },
-	{ 11, 0, "User Identity Module service" },
-	{ 12, 0, "Phonebook Management service" },
-	{ 13, 0, "QCHAT service" },
-	{ 14, 0, "Remote file system service" },
-	{ 15, 0, "Test service" },
-	{ 16, 0, "Location service (~ PDS v2)" },
-	{ 17, 0, "Specific absorption rate service" },
-	{ 18, 0, "IMS settings service" },
-	{ 19, 0, "Analog to digital converter driver service" },
-	{ 20, 0, "Core sound driver service" },
-	{ 21, 0, "Modem embedded file system service" },
-	{ 22, 0, "Time service" },
-	{ 23, 0, "Thermal sensors service" },
-	{ 24, 0, "Thermal mitigation device service" },
-	{ 25, 0, "Service access proxy service" },
-	{ 26, 0, "Wireless data administrative service" },
-	{ 27, 0, "TSYNC control service" },
-	{ 28, 0, "Remote file system access service" },
-	{ 29, 0, "Circuit switched videotelephony service" },
-	{ 30, 0, "Qualcomm mobile access point service" },
-	{ 31, 0, "IMS presence service" },
-	{ 32, 0, "IMS videotelephony service" },
-	{ 33, 0, "IMS application service" },
-	{ 34, 0, "Coexistence service" },
-	{ 36, 0, "Persistent device configuration service" },
-	{ 38, 0, "Simultaneous transmit service" },
-	{ 39, 0, "Bearer independent transport service" },
-	{ 40, 0, "IMS RTP service" },
-	{ 41, 0, "RF radiated performance enhancement service" },
-	{ 42, 0, "Data system determination service" },
-	{ 43, 0, "Subsystem control service" },
-	{ 47, 0, "Data Port Mapper service" },
-	{ 49, 0, "IPA control service" },
-	{ 50, 0, "-- UIMRMT QMI Service "},
-	{ 51, 0, "CoreSight remote tracing service" },
-	{ 52, 0, "Dynamic Heap Memory Sharing" },
-	{ 55, 0, "-- QMI-SLIM service"},
-	{ 56,0, "-- LOWI" },
-	{ 57, 0, "-- WLPS" }, 
-	{ 64, 0, "Service registry locator service" },
-	{ 66, 0, "Service registry notification service" },
-	{ 69, 0, "ATH10k WLAN firmware service" },
-	{ 71, 0, "UIMHTTP" },
-	{ 77, 0, "-- RCS " },
-	{ 224, 0, "Card Application Toolkit service (v1)" },
-	{ 225, 0, "Remote Management Service" },
-	{ 226, 0, "Open Mobile Alliance device management service" },
-	{ 235, 0, "-- MODEM_SVC"}, 
-	{ 256, 0, "-- Sensor services"},
-	{ 312, 0, "QBT1000 Ultrasonic Fingerprint Sensor service" },
-	{ 769, 0, "SLIMbus control service" },
-	{ 771, 0, "Peripheral Access Control Manager service" },
-	{ 4096, 0, "TFTP" },
-	{ DIAG_SERVICE, 0, "DIAG service" },
-};
 
 /* QMI Device handler proto
  *
@@ -294,100 +160,16 @@ static const struct {
  *  First one to check is 47 or 0x2F, which is the DPM, to play
  *  with modem init and request DATA40_CNTL properly
  */
-enum {
-	STATE_INIT = 0,
-	STATE_GOT_CID = 1,
-	STATE_RESET = 2,
-};
 
 struct qmi_device {
 	int32_t fd;
 	struct sockaddr_msm_ipc socket;
 	uint8_t service;
 	uint8_t transaction_id;
-	ctl_state_t ctl_state;
-	uint8_t buf[QMI_DEFAULT_BUF_SIZE];
+	uint8_t buf[MAX_PACKET_SIZE];
 	uint32_t handle;
 	uint8_t cid;
-	uint8_t ctl_num_cids;
-	uint16_t cur_qmux_length;
-	uint16_t qmux_progress;
 };
-
-
-/* From qmi_hdrs.h at qmi-dialer */
-
-//qmux header
-struct qmux_header{
-    uint16_t length;
-    uint8_t control_flags;
-    uint8_t service_type;
-    uint8_t client_id;
-} __attribute__((packed));
-
-//The two different types of QMI headers I have seen. According to the
-//specification, the size of the header is implementation-dependant (depending
-//on service)
-struct qmi_header_ctl{
-    uint8_t control_flags;
-    uint8_t transaction_id;
-    uint16_t message_id;
-    uint16_t length;
-} __attribute__((packed));
-
-struct qmi_header_gen{
-    uint8_t control_flags;
-    uint16_t transaction_id;
-    uint16_t message_id;
-    uint16_t length;
-} __attribute__((packed));
-
-struct qmi_tlv{
-    uint8_t type;
-    uint16_t length;
-} __attribute__((packed));
-
-typedef struct qmux_header qmux_hdr_t;
-typedef struct qmi_header_ctl qmi_hdr_ctl_t;
-typedef struct qmi_header_gen qmi_hdr_gen_t;
-typedef struct qmi_tlv qmi_tlv_t;
-
-#define QMI_DEFAULT_BUF_SIZE    0x1000
-#define QMI_CTL_RELEASE_CID     0x0023
-
-//Control flags
-#define QMI_CTL_FLAGS_RESP      0x3
-#define QMI_CTL_FLAGS_IND       0x4
-
-//Variables
-#define QMI_RESULT_SUCCESS      0x0000
-#define QMI_RESULT_FAILURE      0x0001
-
-//Return values for message passing functions
-#define QMI_MSG_SUCCESS         0x0
-#define QMI_MSG_FAILURE         0x1
-#define QMI_MSG_IGNORE          0x2
-
-//Different sates for each service type
-enum{
-    CTL_NOT_SYNCED = 0,
-    CTL_SYNCED,
-};
-
-
-//CTL message types
-#define QMI_CTL_GET_CID         0x0022
-#define QMI_CTL_RELEASE_CID     0x0023
-#define QMI_CTL_SET_DATA_FORMAT	0x0026
-#define QMI_CTL_SYNC            0x0027
-
-//CTL TLVs
-#define QMI_CTL_TLV_ALLOC_INFO  0x01
-
-//Set data format
-#define	QMI_CTL_TLV_DATA_FORMAT	0x01
-#define QMI_CTL_TLV_DATA_PROTO	0x10
-
 
 struct atcmd_reg_request {
 	// QMI Header
@@ -405,8 +187,38 @@ struct atcmd_reg_request {
 	uint16_t dummy3; // always 0x00 0x01
 	uint8_t strlength;// var3;
 	char *command; // The command itself (+CFUN, +QDAI...)
-//	uint8_t fillzero[47]; // Fills 47 bytes with zeroes on _every_ command...
 	
 } __attribute__((packed));
+
+struct at_command_modem_response {
+	// QMI Header
+	uint8_t ctlid; // 0x04
+	uint16_t transaction_id; // incremental counter for each request, why 1 in its response? separate counter?
+	uint16_t msgid; // 0x21 0x00// RESPONSE!
+	uint16_t length; // 0x71 0x00 Sizeof the entire packet
+
+	// the request packet itself
+	// Obviously unfinished :)
+	uint8_t dummy1; // always 0x00 0x01
+	uint8_t var1; // 0x12?
+	uint16_t dummy2; // always 0x00 0x01
+	uint16_t dummy3; // always 0x00 0x00
+	uint16_t dummy4; // always 0x00 0x01
+  uint16_t dummy5; // 0x00 0x00
+	uint8_t cmd_len; // 0x09?
+  char *command;
+  // The rest I have no idea yet, more sample packets are needed..
+} __attribute__((packed));
+
+struct at_command_simple_reply {
+	uint8_t ctlid; // 0x04
+	uint16_t transaction_id; // incremental counter for each request, why 1 in its response? separate counter?
+	uint16_t msgid; // 0x21 0x00// RESPONSE!
+	uint16_t length; // 0x71 0x00 Sizeof the entire packet
+	uint16_t command;
+	uint16_t result;
+	uint16_t response;
+	unsigned char *raw_response;
+};
 
 #endif
