eedb72fcca5fb68ada4fee916b5edaab9b5e1d9c
Biktor <biktor@biktorgj.com>
2021-04-09 07:46:58 +0200

    Initial QDAI Setting support

--- recipes-modem/openqti/files/src/openqti.c
+++ recipes-modem/openqti/files/src/openqti.c
@@ -544,6 +544,157 @@ void switch_adb(bool en) {
     logger(MSG_ERROR, "%s: Error enabling USB \n", __func__);
   }
 }
+// AT+QDAI=1,1,0,1,0,0,1,1
+int set_audio_profile(uint8_t io, uint8_t mode, uint8_t fsync, uint8_t clock,
+                      uint8_t format, uint8_t sample, uint8_t num_slots,
+                      uint8_t slot) {
+  uint8_t ret = 0;
+  /* We know we don't have an analog codec here,
+     so EINVAL if anything other than Digital PCM (i2s) */
+  logger(MSG_ERROR, "Set audio profile: 0x%.2x 0x%.2x 0x%.2x 0x%.2x 0x%.2x 0x%.2x 0x%.2x 0x%.2x\n", io, mode, fsync, clock, format, sample, num_slots, slot);
+  switch (io) { // 1,2,3,5, Digital PCM, <--analog-->
+  case 0x31:
+    if (write_to(sysfs_value_pairs[0].path, "0", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[0].path);
+      ret = -EINVAL;
+    }
+    break;
+  default:
+    ret = -EINVAL;
+    break;
+  }
+  switch (mode) { // Master / Slave
+  case 0x30:      // Master (they're flipped?)
+    if (write_to(sysfs_value_pairs[2].path, "1", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[1].path);
+      ret = -EINVAL;
+    }
+    break;
+  case 0x31: // Slave
+    if (write_to(sysfs_value_pairs[2].path, "0", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[1].path);
+      ret = -EINVAL;
+    }
+    break;
+    break;
+  default:
+    ret = -EINVAL;
+    break;
+  }
+  switch (fsync) { // Short sync / Long sync
+  case 0x30:
+    if (write_to(sysfs_value_pairs[1].path, "0", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[1].path);
+      ret = -EINVAL;
+    }
+    break;
+  case 0x31:
+    if (write_to(sysfs_value_pairs[1].path, "1", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[1].path);
+      ret = -EINVAL;
+    }
+    break;
+  default:
+    return -EINVAL;
+    break;
+  }
+  switch (clock) { // Clock freq
+  case 0x30:       // 128
+    if (write_to(sysfs_value_pairs[5].path, "128000", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[5].path);
+      ret = -EINVAL;
+    }
+    break;
+  case 0x31: // 256
+    if (write_to(sysfs_value_pairs[5].path, "256000", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[5].path);
+      ret = -EINVAL;
+    }
+    break;
+  case 0x32: // 512
+    if (write_to(sysfs_value_pairs[5].path, "512000", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[5].path);
+      ret = -EINVAL;
+    }
+    break;
+  case 0x33: // 1024
+    if (write_to(sysfs_value_pairs[5].path, "1024000", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[5].path);
+      ret = -EINVAL;
+    }
+    break;
+  case 0x34: // 2048
+    if (write_to(sysfs_value_pairs[5].path, "2048000", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[5].path);
+      ret = -EINVAL;
+    }
+    break;
+  case 0x35: // 4096
+    if (write_to(sysfs_value_pairs[5].path, "4096000", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[5].path);
+      ret = -EINVAL;
+    }
+    break;
+  default:
+    ret = -EINVAL;
+    break;
+  }
+  switch (format) {
+  case 0x30: // 16bit linear
+  case 0x31: // 8bit a-law
+  case 0x32: // 8bit u-law
+    break;
+  default:
+    ret = -EINVAL;
+    break;
+  }
+  switch (sample) { // Sample rate
+  case 0x30:        // 8k
+    if (write_to(sysfs_value_pairs[6].path, "8000", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[6].path);
+      ret = -EINVAL;
+    }
+    // Write bits per frame too. Anything less than 5 for 8k
+    if (write_to(sysfs_value_pairs[3].path, "0", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[3].path);
+      ret = -EINVAL;
+    }
+  case 0x31: // 16k
+    if (write_to(sysfs_value_pairs[6].path, "16000", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[6].path);
+      ret = -EINVAL;
+    }
+    /* Per msm-dai-q6-v2.c, use frame > AFE_PORT_PCM_BITS_PER_FRAME_256
+       to specify PCM16k mode
+    */
+    if (write_to(sysfs_value_pairs[3].path, "6", O_RDWR) < 0) {
+      logger(MSG_ERROR, "%s: Error writing to %s\n", __func__,
+             sysfs_value_pairs[3].path);
+      ret = -EINVAL;
+    }
+    break;
+  default:
+    ret = -EINVAL;
+    break;
+  }
+  /* Number of slots and current slot are not used */
+
+  return ret;
+}
 /* When a command is requested via AT interface,
    this function is used to answer to them */
 int handle_atfwd_response(struct qmi_device *qmidev, uint8_t *buf,
@@ -600,20 +751,20 @@ int handle_atfwd_response(struct qmi_device *qmidev, uint8_t *buf,
   cmdreply->response = 3;
   cmdreply->result = 1;
 
-  // Fallback for dummy commands that arent implemented
-  if ((cmd_id > 0 && cmd_id < 72) || (cmd_id > 72 && cmd_id < 108)) {
-    cmdreply->result = 1;
-    sckret =
-        sendto(qmidev->fd, cmdreply, sizeof(struct at_command_simple_reply),
-               MSG_DONTWAIT, (void *)&qmidev->socket, sizeof(qmidev->socket));
-  }
-
   switch (cmd_id) {
-  case 111: // QCPowerdown
+  case 40: // QDAI
+    if (sz > 60) {
+      sckret = set_audio_profile(buf[30], buf[35], buf[40], buf[45], buf[50],
+                                 buf[55], buf[60], buf[65]);
+      if (sckret < 0) {
+        cmdreply->result = 2;
+      }
+    } else {
+      cmdreply->result = 2;
+    }
     sckret =
         sendto(qmidev->fd, cmdreply, sizeof(struct at_command_simple_reply),
                MSG_DONTWAIT, (void *)&qmidev->socket, sizeof(qmidev->socket));
-    reboot(0x4321fedc);
     break;
   case 72: // fastboot
     sckret =
@@ -622,18 +773,11 @@ int handle_atfwd_response(struct qmi_device *qmidev, uint8_t *buf,
     set_next_fastboot_mode(0);
     reboot(0x01234567);
     break;
-  case 115: // reboot to recovery
+  case 111: // QCPowerdown
     sckret =
         sendto(qmidev->fd, cmdreply, sizeof(struct at_command_simple_reply),
                MSG_DONTWAIT, (void *)&qmidev->socket, sizeof(qmidev->socket));
-    set_next_fastboot_mode(1);
-    reboot(0x01234567);
-    break;
-  case 116:
-    cmdreply->result = 1;
-    sckret =
-        sendto(qmidev->fd, cmdreply, sizeof(struct at_command_simple_reply),
-               MSG_DONTWAIT, (void *)&qmidev->socket, sizeof(qmidev->socket));
+    reboot(0x4321fedc);
     break;
   case 109:
     sckret =
@@ -664,10 +808,28 @@ int handle_atfwd_response(struct qmi_device *qmidev, uint8_t *buf,
         sendto(qmidev->fd, cmdreply, sizeof(struct at_command_simple_reply),
                MSG_DONTWAIT, (void *)&qmidev->socket, sizeof(qmidev->socket));
     break;
+  case 115: // reboot to recovery
+    sckret =
+        sendto(qmidev->fd, cmdreply, sizeof(struct at_command_simple_reply),
+               MSG_DONTWAIT, (void *)&qmidev->socket, sizeof(qmidev->socket));
+    set_next_fastboot_mode(1);
+    reboot(0x01234567);
+    break;
+  case 116:
+    cmdreply->result = 1;
+    sckret =
+        sendto(qmidev->fd, cmdreply, sizeof(struct at_command_simple_reply),
+               MSG_DONTWAIT, (void *)&qmidev->socket, sizeof(qmidev->socket));
+    break;
 
   default:
-    logger(MSG_ERROR, "%s: Unknown command requested \n", __func__);
-    cmdreply->result = 2;
+    // Fallback for dummy commands that arent implemented
+    if ((cmd_id > 0 && cmd_id < 72) || (cmd_id > 72 && cmd_id < 108)) {
+      cmdreply->result = 1;
+    } else {
+      logger(MSG_ERROR, "%s: Unknown command requested \n", __func__);
+      cmdreply->result = 2;
+    }
     sckret =
         sendto(qmidev->fd, cmdreply, sizeof(struct at_command_simple_reply),
                MSG_DONTWAIT, (void *)&qmidev->socket, sizeof(qmidev->socket));
