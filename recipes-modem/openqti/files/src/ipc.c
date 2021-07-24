967fbf9db2a041dd89c8689112721afb6da7fc89
Biktor <biktor@biktorgj.com>
2021-07-24 05:58:21 +0200

    Add logic for client release tracking from the host, so we don't fill the list but fail to drain it -affecting older mm versions at least-

--- recipes-modem/openqti/files/src/ipc.c
+++ recipes-modem/openqti/files/src/ipc.c
@@ -9,15 +9,14 @@
 #include <unistd.h>
 
 #include "../inc/devices.h"
+#include "../inc/helpers.h"
 #include "../inc/ipc.h"
 #include "../inc/logger.h"
-#include "../inc/helpers.h"
 struct {
   uint8_t services[32];
   uint8_t last_active;
   uint32_t regtime;
 } client_handle_track;
-
 
 int open_ipc_socket(struct qmi_device *qmisock, uint32_t node, uint32_t port,
                     uint32_t service, uint32_t instance,
@@ -301,45 +300,39 @@ void drain_client_tracking() {
   int i;
   client_handle_track.regtime = 0;
   client_handle_track.last_active = 0;
-  for (i = 0; i <32 ; i++) {
+  for (i = 0; i < 32; i++) {
     client_handle_track.services[i] = 0;
   }
 }
 int track_client_count(uint8_t *pkt, int from, int sz) {
-  int msglength;
-  // get_curr_timestamp
-  /* mm -> dsp: 0x01 0x0f 0x00 0x00 0x00 0x00 0x00 0x02 0x22 0x00 0x04 0x00 0x01
-   * 0x01 0x00 0x1a  */
-  /* response: 0x01 0x17 0x00 0x80 0x00 0x00 0x01 0x02 0x22 0x00 0x0c 0x00 0x02
-   * 0x04 0x00 0x00 0x00 0x00 0x00 0x01 0x02 0x00 0x1a 0x01 */
+  int msglength, i;
   if (pkt[0] != 0x01 || sz < 12 || pkt[9] != 0x00) {
-    //  logger(MSG_WARN, "%s: Don't care about this packet\n", __func__);
     return 0;
   }
-
 
   switch (pkt[8]) {
   case 0x22:
     logger(MSG_WARN, "%s: QMI Register client request\n", __func__);
     if (client_handle_track.regtime == 0) {
       client_handle_track.regtime = get_curr_timestamp();
-    /*
-     * We need some headroom for mm/ofono here. Some services are registered
-     * only after the user has entered the PIN, so wait at least 240 seconds
-     * to trigger a client release. I'll make this better at some point when
-     * I find all the services triggered from the PIN entry
-     */
+      /*
+       * We need some headroom for mm/ofono here. Some services are registered
+       * only after the user has entered the PIN, so wait at least 240 seconds
+       * to trigger a client release. I'll make this better at some point when
+       * I find all the services triggered from the PIN entry
+       */
     } else if ((get_curr_timestamp() - client_handle_track.regtime) > 240000) {
-      //Needs a force reset
-     logger(MSG_WARN, "%s: It seems we need a reset \n", __func__ );
-     return 1;
+      // Needs a force reset
+      logger(MSG_WARN, "%s: It seems we need a reset \n", __func__);
+      return 1;
     }
     msglength = pkt[10] + 10;
     switch (from) {
     case FROM_DSP:
       logger(MSG_WARN, "%s: Assigned instance ID 0x%.2x to service 0x%.2x \n",
-             __func__, pkt[msglength+1], pkt[msglength]);
-      client_handle_track.services[client_handle_track.last_active] = pkt[msglength];
+             __func__, pkt[msglength + 1], pkt[msglength]);
+      client_handle_track.services[client_handle_track.last_active] =
+          pkt[msglength];
       client_handle_track.last_active++;
       break;
     case FROM_HOST:
@@ -359,10 +352,25 @@ int track_client_count(uint8_t *pkt, int from, int sz) {
     switch (from) {
     case FROM_DSP:
       logger(MSG_WARN, "%s: QMI Client Release from DSP\n", __func__);
-
       break;
     case FROM_HOST:
-      logger(MSG_WARN, "%s: QMI Client Release from HOST\n", __func__);
+      /* If the host removes a port free it too */
+      logger(MSG_WARN,
+             "%s: QMI Client Release from HOST,S:%.2x I:%.2x, AC:%i \n",
+             __func__, pkt[15], pkt[16], client_handle_track.last_active);
+      for (i = 32; i >= 0; i--) {
+        if (client_handle_track.services[i] == pkt[15]) {
+          client_handle_track.services[i] = 0;
+          client_handle_track.last_active--;
+          if (client_handle_track.last_active <= 0) {
+            logger(MSG_WARN,
+                   "%s: All QMI Clients have been freed from the host\n",
+                   __func__);
+            client_handle_track.last_active = 0;
+            client_handle_track.regtime = 0;
+          }
+        }
+      }
       break;
     }
     break;
@@ -372,19 +380,24 @@ int track_client_count(uint8_t *pkt, int from, int sz) {
 
 void force_close_qmi(int fd) {
   int transaction_id = 0;
-  int ret,i, instance;
-  uint8_t release_prototype[] = {0x01, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x04, 0x23, 0x00, 0x05, 0x00, 0x01, 0x02, 0x00, 0x1a, 0x01};
+  int ret, i, instance;
+  uint8_t release_prototype[] = {0x01, 0x10, 0x00, 0x00, 0x00, 0x00,
+                                 0x00, 0x04, 0x23, 0x00, 0x05, 0x00,
+                                 0x01, 0x02, 0x00, 0x1a, 0x01};
   logger(MSG_WARN, "%s: Closing all active QMI connections\n", __func__);
   for (i = 0; i < client_handle_track.last_active; i++) {
-    for (instance = 0; instance <= 0x05; instance++ ) {
+    for (instance = 0; instance <= 0x05; instance++) {
       release_prototype[7] = transaction_id;
       transaction_id++;
       release_prototype[15] = client_handle_track.services[i];
       release_prototype[16] = instance;
 
       ret = write(fd, release_prototype, sizeof(release_prototype));
-      logger(MSG_WARN, "%s: Closing connection to service %.2x, instance %i, bytes written: %i \n", __func__, client_handle_track.services[i], instance, ret);
-    } 
+      logger(MSG_DEBUG,
+             "%s: Closing connection to service %.2x, instance %i, bytes "
+             "written: %i \n",
+             __func__, client_handle_track.services[i], instance, ret);
+    }
   }
   drain_client_tracking();
 }
