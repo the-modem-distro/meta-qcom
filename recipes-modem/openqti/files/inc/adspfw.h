680b96464853979fb1f5dc1f7db171a559cc9455
Biktor <biktor@biktorgj.com>
2022-06-05 06:51:01 +0200

    Add checksum for .006 firmware

--- recipes-modem/openqti/files/inc/adspfw.h
+++ recipes-modem/openqti/files/inc/adspfw.h
@@ -21,6 +21,7 @@ static const struct {
     {4, '1', "EG25GGBR07A07M2G_01.001.02", "6a92799327a56905f6496613671f56df"},
     {5, '1', "EG25GGBR07A07M2G_OCPU_01.001.01.001", "00f6763a96bc702eaf710cc928b6fa99"},
     {6, '4', "EG25GGBR07A08M2G_30.004.30.004", "74fcbcdd4abed713ce8cd3f8818e7afd"},   
+    {7, '4', "EG25GGBR07A08M2G_30.006.30.006", "2206b35df4cc1c51a06b1bb25729a0cd"},   
 };
 
 #endif
