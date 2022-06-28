453c7b361559f36b8258f8444675d9c4530b9e0b
Biktor <biktor@biktorgj.com>
2022-06-28 07:00:12 +0200

    Thermal management fixes: Highest threshold before it begins complaining Less logging (once every 10 seconds) Separate logging for temperature monitoring and the rest of openqti

--- recipes-modem/openqti/files/inc/logger.h
+++ recipes-modem/openqti/files/inc/logger.h
@@ -9,9 +9,14 @@
 #define VOLATILE_LOGPATH "/var/log/openqti.log"
 #define PERSISTENT_LOGPATH "/persist/openqti.log"
 
+
+#define VOLATILE_THERMAL_LOGFILE "/var/log/thermal.log"
+#define PERSISTENT_THERMAL_LOGFILE "/persist/thermal.log"
+
 void reset_logtime();
 double get_elapsed_time();
 void logger(uint8_t level, char *format, ...);
+void log_thermal_status(uint8_t level, char *format, ...);
 void dump_packet(char *direction, uint8_t *buf, int pktsize);
 uint8_t get_log_level();
 void set_log_level(uint8_t level);
@@ -19,3 +24,4 @@ void set_log_method(bool ttyout);
 void dump_pkt_raw(uint8_t *buf, int pktsize);
 int mask_phone_number(uint8_t *orig, char *dest, uint8_t len);
 #endif
+
