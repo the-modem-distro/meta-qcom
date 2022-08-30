e817baf513bfc15b3c27cc847679f8564b8d2195
Biktor <biktor@biktorgj.com>
2022-08-30 07:32:21 +0200

    MBNLoader: redo prototype. This one mostly works, albeit a little stubborn if ModemManager is attached to the serial port

--- recipes-modem/mbnloader/files/src/mbnloader.h
+++ recipes-modem/mbnloader/files/src/mbnloader.h
@@ -6,11 +6,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-#define SMD_DEVICE_PATH "/dev/ttyS2"
+#define SMD_DEVICE_PATH "/dev/ttyUSB2"
 #define UPLOAD_CMD "AT+QFUPL=" //"RAM:test.mbn",40160
 #define DELETE_CMD "AT+QFDEL="
-#define LOAD_AS_MBN_CMD "AT+QMBNCFG=\"Add\",\"volte_profile.mbn\"\r\n"
-#define SET_AUTOSEL_CMD "AT+QMBNCFG=\"Autosel\",1\r\n"
+#define LOAD_AS_MBN_CMD "AT+QMBNCFG=\"Add\",\"volte_profile.mbn\"\n\r"
+#define SET_AUTOSEL_CMD "AT+QMBNCFG=\"Autosel\",1\n\r"
 #define LIST_FILES_CMD "AT+QMBNCFG=\"List\""
 
 #endif
