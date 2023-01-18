93fec9a34c98a682beed5cc6fc4cdc4e4949c304
Biktor <biktor@biktorgj.com>
2023-01-18 09:47:58 +0100

    [WIP] Add an internal QMI client. This commit includes some sample message request to ask the baseband for the manufacturer, serial number, time and revision of the baseband. Looking to get the baseband version string via QMI to remove adspfw.h

--- recipes-modem/openqti/files/src/qmi.c
+++ recipes-modem/openqti/files/src/qmi.c
@@ -2,15 +2,27 @@
 #include "../inc/qmi.h"
 #include "../inc/call.h"
 #include "../inc/cell.h"
+#include "../inc/devices.h"
+#include "../inc/ipc.h"
 #include "../inc/logger.h"
 #include "../inc/openqti.h"
 #include "../inc/sms.h"
+
 #include <endian.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
 #include <time.h>
 #include <unistd.h>
+
+struct {
+  uint8_t is_initialized;
+  int fd;
+  struct qmi_service_bindings services[QMI_SERVICES_LAST];
+} internal_qmi_client;
 
 /*
  *
@@ -35,25 +47,27 @@ uint8_t get_qmux_instance_id(void *bytes, size_t len) {
 }
 /* Get Message ID from a QMI control message*/
 uint16_t get_control_message_id(void *bytes, size_t len) {
-  struct ctl_qmi_packet *pkt = (struct ctl_qmi_packet *)(bytes + sizeof(struct qmux_packet));
-  logger(MSG_DEBUG, "Control message: CTL %.2x | TID: %.2x | MSG %.4x | LEN %.4x\n", pkt->ctlid, pkt->transaction_id, pkt->msgid, pkt->length);
+  struct ctl_qmi_packet *pkt =
+      (struct ctl_qmi_packet *)(bytes + sizeof(struct qmux_packet));
+  logger(MSG_DEBUG,
+         "Control message: CTL %.2x | TID: %.2x | MSG %.4x | LEN %.4x\n",
+         pkt->ctlid, pkt->transaction_id, pkt->msgid, pkt->length);
   return pkt->msgid;
 }
 
 /* Get Message ID for a QMI message (from a service) */
 uint16_t get_qmi_message_id(void *bytes, size_t len) {
-  struct qmi_packet *pkt = (struct qmi_packet *)(bytes + sizeof(struct qmux_packet));
-  logger(MSG_DEBUG, "QMI message: CTL %.2x | TID: %.2x | MSG %.4x | LEN %.4x\n", pkt->ctlid, pkt->transaction_id, pkt->msgid, pkt->length);
+  struct qmi_packet *pkt =
+      (struct qmi_packet *)(bytes + sizeof(struct qmux_packet));
   return pkt->msgid;
 }
 
 /* Get Message ID for a QMI message (from a service) */
 uint16_t get_qmi_transaction_id(void *bytes, size_t len) {
-  struct qmi_packet *pkt = (struct qmi_packet *)(bytes + sizeof(struct qmux_packet));
-  logger(MSG_DEBUG, "QMI message: CTL %.2x | TID: %.2x | MSG %.4x | LEN %.4x\n", pkt->ctlid, pkt->transaction_id, pkt->msgid, pkt->length);
+  struct qmi_packet *pkt =
+      (struct qmi_packet *)(bytes + sizeof(struct qmux_packet));
   return pkt->transaction_id;
 }
-
 
 /* Get Transaction ID for a QMI message */
 uint16_t get_transaction_id(void *bytes, size_t len) {
@@ -101,7 +115,7 @@ uint16_t get_tlv_offset_by_id(uint8_t *bytes, size_t len, uint8_t tlvid) {
 }
 
 /*
- * Gets error string from a result_code inside a QMI indication 
+ * Gets error string from a result_code inside a QMI indication
  * TLV (0x02)
  */
 const char *get_qmi_error_string(uint16_t result_code) {
@@ -164,14 +178,18 @@ uint16_t did_qmi_op_fail(uint8_t *bytes, size_t len) {
   return result;
 }
 
-int build_qmux_header(void *output, size_t output_len, uint8_t control, uint8_t service, uint8_t instance) {
+int build_qmux_header(void *output, size_t output_len, uint8_t control,
+                      uint8_t service, uint8_t instance) {
   if (output_len < sizeof(struct qmux_packet)) {
-    logger(MSG_ERROR, "%s: Can't build QMUX header, buffer is too small\n", __func__);
+    logger(MSG_ERROR, "%s: Can't build QMUX header, buffer is too small\n",
+           __func__);
     return -ENOMEM;
   }
   struct qmux_packet *pkt = (struct qmux_packet *)output;
   pkt->version = 0x01;
-  pkt->packet_length = output_len - sizeof(uint8_t); // QMUX's stated length is entire size - version field
+  pkt->packet_length =
+      output_len -
+      sizeof(uint8_t); // QMUX's stated length is entire size - version field
   pkt->control = control;
   pkt->service = service;
   pkt->instance_id = instance;
@@ -179,16 +197,201 @@ int build_qmux_header(void *output, size_t output_len, uint8_t control, uint8_t 
   return 0;
 }
 
-int build_qmi_header(void *output, size_t output_len, uint8_t ctlid, uint16_t transaction_id, uint16_t message_id) {
+int build_qmi_header(void *output, size_t output_len, uint8_t ctlid,
+                     uint16_t transaction_id, uint16_t message_id) {
   if (output_len < (sizeof(struct qmux_packet) + sizeof(struct qmi_packet))) {
-    logger(MSG_ERROR, "%s: Can't build QMI header, buffer is too small\n", __func__);
+    logger(MSG_ERROR, "%s: Can't build QMI header, buffer is too small\n",
+           __func__);
     return -ENOMEM;
   }
-  struct qmi_packet *pkt = (struct qmi_packet *)(output + sizeof(struct qmux_packet));
+  struct qmi_packet *pkt =
+      (struct qmi_packet *)(output + sizeof(struct qmux_packet));
   pkt->ctlid = ctlid;
   pkt->transaction_id = transaction_id;
   pkt->msgid = message_id;
-  pkt->length = output_len - sizeof(struct qmux_packet) - sizeof(struct qmi_packet);
+  pkt->length =
+      output_len - sizeof(struct qmux_packet) - sizeof(struct qmi_packet);
   pkt = NULL;
   return 0;
+}
+/* INTERNAL QMI CLIENT */
+size_t read_from_qmi_port(void *buff, size_t max_sz) {
+  size_t bytes_read = 0;
+  fd_set readfds;
+  struct timeval tv;
+
+  memset(buff, 0, max_sz);
+  tv.tv_sec = 0;
+  tv.tv_usec = 500000;
+  FD_SET(internal_qmi_client.fd, &readfds);
+  select(MAX_FD, &readfds, NULL, NULL, &tv);
+  if (FD_ISSET(internal_qmi_client.fd, &readfds)) {
+    logger(MSG_INFO, "%s: Is set!\n", __func__);
+    bytes_read = read(internal_qmi_client.fd, buff, max_sz);
+  }
+  set_log_level(0);
+  dump_pkt_raw(buff, bytes_read);
+  set_log_level(1);
+  logger(MSG_INFO, "%s: Bytes read: %u\n", __func__, bytes_read);
+  return bytes_read;
+}
+
+size_t write_to_qmi_port(void *buff, size_t bufsz) {
+  size_t bytes_written = 0;
+  set_log_level(0);
+  dump_pkt_raw(buff, bufsz);
+  set_log_level(1);
+  bytes_written = write(internal_qmi_client.fd, buff, bufsz);
+  if (bytes_written != bufsz) {
+    logger(MSG_WARN,
+           "%s: Heey we wrote %u bytes from %u that we were supposed to...\n",
+           __func__, bytes_written, bufsz);
+  }
+  return bytes_written;
+}
+
+int allocate_qmi_client(uint8_t service) {
+  uint8_t *buff = malloc(1024 * sizeof(uint8_t));
+  struct client_alloc_request *request = NULL;
+  size_t bytes = 0;
+  int ret = 0;
+  if (!internal_qmi_client.is_initialized) {
+    internal_qmi_client.fd = open(INT_SMD_CNTL, O_RDWR);
+    if (internal_qmi_client.fd < 0) {
+      logger(MSG_ERROR, "Error opening %s!\n", INT_SMD_CNTL);
+      free(buff);
+      return -EINVAL;
+    } else {
+      logger(MSG_INFO, "%s: Opened internal SMD port\n", __func__);
+      internal_qmi_client.is_initialized = 1;
+    }
+  }
+
+  request = (struct client_alloc_request *)buff;
+  request->qmux.version = 0x01;
+  request->qmux.packet_length = 0x0f;
+  request->qmux.service = 0x00;
+  request->qmux.instance_id = 0x00;
+
+  request->qmi.id = 0x06;
+  request->qmi.msgid = 0x0022;
+  request->qmi.size = 0x04;
+
+  request->service.id = 0x01;
+  request->service.len = 0x01;
+  request->service.service_id = service; // 0x02 -> dms
+  if (write_to_qmi_port((void *)request, sizeof(struct client_alloc_request)) <
+      sizeof(struct client_alloc_request)) {
+    logger(MSG_ERROR, "%s: Allocation failed\n", __func__);
+    free(buff);
+    return -ENOMEM;
+  }
+  memset(buff, 0, 1024);
+  bytes = read_from_qmi_port(buff, 1024);
+  if (bytes >= sizeof(struct client_alloc_response)) {
+    logger(MSG_INFO, "We may have a valid response, let's check it\n");
+    struct client_alloc_response *response =
+        (struct client_alloc_response *)buff;
+    if (response->result.response == 0x00 && response->result.result == 0x00) {
+      logger(MSG_INFO, "%s: Yesss we allocated or client, instance ID: %.2x\n",
+             __func__, response->instance.instance_id);
+      internal_qmi_client.services[service].service =
+          response->instance.service_id;
+      internal_qmi_client.services[service].instance =
+          response->instance.instance_id;
+      internal_qmi_client.services[service].curr_transaction_id =
+          response->qmi.transaction_id;
+      internal_qmi_client.services[service].is_initialized = 1;
+    }
+  } else {
+    logger(MSG_ERROR, "%s: Invalid allocation response: %u bytes\n", __func__,
+           bytes);
+  }
+  free(buff);
+  return ret;
+}
+
+void *init_internal_qmi_client() {
+  int ret;
+  fd_set readfds;
+  uint8_t buf[MAX_PACKET_SIZE];
+  struct timeval tv;
+  uint8_t sample_demo = 0;
+  size_t demo_pkt_len = sizeof(struct qmux_packet) + sizeof(struct qmi_packet);
+  uint8_t *demo_pkt = malloc(demo_pkt_len);
+  logger(MSG_WARN, "%s Trying to register to WDS\n", __func__);
+  if (allocate_qmi_client(QMI_SERVICE_WDS) < 0) {
+    logger(MSG_ERROR, "%s: Failed to allocate client: QMI_SERVICE_WDS\n",
+           __func__);
+  } else {
+    logger(MSG_INFO, "%s: Connected to QMI_SERVICE_WDS\n", __func__);
+  }
+
+  logger(MSG_WARN, "%s Trying to register to DMS\n", __func__);
+  if (allocate_qmi_client(QMI_SERVICE_DMS) < 0) {
+    logger(MSG_ERROR, "%s: Failed to allocate client: QMI_SERVICE_DMS\n",
+           __func__);
+  } else {
+    logger(MSG_INFO, "%s: Connected to QMI_SERVICE_DMS\n", __func__);
+  }
+
+  while (1) {
+    logger(MSG_INFO, "%s: loop\n", __func__);
+    FD_SET(internal_qmi_client.fd, &readfds);
+    tv.tv_sec = 0;
+    tv.tv_usec = 500000;
+    select(MAX_FD, &readfds, NULL, NULL, &tv);
+    if (FD_ISSET(internal_qmi_client.fd, &readfds)) {
+      ret = read(internal_qmi_client.fd, &buf, MAX_PACKET_SIZE);
+      logger(MSG_INFO, "%s: New QMI Message of %i bytes\n", __func__, ret);
+      pretty_print_qmi_pkt("Baseband --> Host", buf, ret);
+    }
+    memset(demo_pkt, 0, demo_pkt_len);
+    if (build_qmux_header(
+            demo_pkt, demo_pkt_len, 0x00, QMI_SERVICE_DMS,
+            internal_qmi_client.services[QMI_SERVICE_DMS].instance) < 0) {
+      logger(MSG_ERROR, "%s: Error adding the qmux header\n", __func__);
+    }
+    switch (sample_demo) {
+    case 0:
+      if (build_qmi_header(demo_pkt, demo_pkt_len, 0x00, 0,
+                           DMS_GET_REVISION) < 0) {
+        logger(MSG_ERROR, "%s: Error adding the qmi header\n", __func__);
+      }
+      break;
+    case 1:
+      if (build_qmi_header(demo_pkt, demo_pkt_len, 0x00, 1,
+                           DMS_GET_SERIAL_NUM) < 0) {
+        logger(MSG_ERROR, "%s: Error adding the qmi header\n", __func__);
+      }
+      break;
+    case 2:
+      if (build_qmi_header(demo_pkt, demo_pkt_len, 0x00, 2,
+                           DMS_GET_MODEL) < 0) {
+        logger(MSG_ERROR, "%s: Error adding the qmi header\n", __func__);
+      }
+      break;
+    case 3:
+      if (build_qmi_header(demo_pkt, demo_pkt_len, 0x00, 3,
+                           DMS_GET_TIME) < 0) {
+        logger(MSG_ERROR, "%s: Error adding the qmi header\n", __func__);
+      }
+      break;
+
+    default:
+      break;
+    }
+    if (sample_demo < 4) {
+      logger(MSG_INFO, "Writing test %i\n", sample_demo);
+      write_to_qmi_port(demo_pkt, demo_pkt_len);
+    }
+    sample_demo++;
+    if (sample_demo > 3) {
+      sample_demo = 0;
+      logger(MSG_INFO, "Restarting demo in 5s\n");
+      sleep(5);
+    }
+  }
+
+  return NULL;
 }
