0bccddc42ac0323e6c1dcfba811e327fc73f643f
Biktor <biktor@biktorgj.com>
2023-01-18 09:52:24 +0100

    [scheduler] Mark tasks as DONE when they're done

--- recipes-modem/openqti/files/src/scheduler.c
+++ recipes-modem/openqti/files/src/scheduler.c
@@ -225,12 +225,15 @@ int run_task(int taskID) {
   case TASK_TYPE_WAKE_HOST:
     logger(MSG_INFO, "%s: Try to wake up the host\n", __func__);
     pulse_ring_in();
+    sch_runtime.tasks[taskID].status = STATUS_DONE;
     break;
   case TASK_TYPE_DND_CLEAR:
     logger(MSG_INFO, "%s: Clear do not disturb mode\n", __func__);
     set_do_not_disturb(false);
+    sch_runtime.tasks[taskID].status = STATUS_DONE;
     break;
   }
+
   cleanup_tasks();
   return 0;
 }
