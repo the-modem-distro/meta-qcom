11f379d2541958e3d55ee85d925f2791fcb073d8
Biktor <biktor@biktorgj.com>
2023-01-21 06:23:12 +0100

    [wds client] Use the QMI client, and add all Message IDs from libqmi

--- recipes-modem/openqti/files/inc/wds.h
+++ recipes-modem/openqti/files/inc/wds.h
@@ -8,7 +8,7 @@
 #include <stdio.h>
 /*
  * Headers for the Wireless data service
- *
+ *	https://gitlab.freedesktop.org/mobile-broadband/libqmi/
  *
  *
  */
@@ -16,84 +16,223 @@
 #define DEFAULT_APN_NAME "internet"
 
 enum {
-    WDS_RESET = 0x0000,
-    WDS_SET_EVENT_REPORT = 0x0001,
-    WDS_ABORT = 0x0002,
-    WDS_IND_REGISTER = 0x0003,
-    WDS_GET_SUPPORTED_MSG = 0x001e,
-    WDS_GET_PROFILE_LIST = 0x002a,
-    WDS_GET_PROFILE_SETTINGS = 0x002b,
-    WDS_START_NETWORK_IF = 0x0020,
-    WDS_STOP_NETWORK_IF = 0x0021,
-    WDS_BIND_MUX_DATA_PORT = 0x00a2,
-    WDS_GET_AUTOCONNECT_SETTING = 0x0034,
-    WDS_EVENT_IFACE_RF_CHANGED = 0x0008,
-    WDS_EVENT_IFACE_DOS_ACK = 0x0009,
-    WDS_GET_LTE_ATTACH_PARAMS = 0x0085,
-    WDS_GET_LTE_ATTACH_PDN_LIST = 0x0094,
-
+  WDS_APN_TYPE_UNKNOWN = 0,
+  WDS_APN_TYPE_INTERNET = 1,
+  WDS_APN_TYPE_IMS = 2,
+  WDS_APN_TYPE_VIRTUAL_SIM = 3,
 };
 
 enum {
-    WDS_APN_TYPE_UNKNOWN = 0,
-    WDS_APN_TYPE_INTERNET = 1,
-    WDS_APN_TYPE_IMS = 2,
-    WDS_APN_TYPE_VIRTUAL_SIM = 3,
+  WDS_EXTENDED_ERROR_CODE = 0xE0,
+  WDS_PROFILE_IDENTIFIER = 0x01,
+  WDS_PROFILE_NAME = 0x10,
+  WDS_PDP_TYPE = 0x11,
+  WDS_PDP_HEADER_COMPRESSION_TYPE = 0x12,
+  WDS_PDP_DATA_COMPRESSION_TYPE = 0x13,
+  WDS_APN_NAME = 0x14,
+  WDS_PRIMARY_IPV4_DNS_ADDRESS = 0x15,
+  WDS_SECONDARY_IPV4_DNS_ADDRESS = 0x16,
+  WDS_UMTS_REQUESTED_QOS = 0x17,
+  WDS_UMTS_MINIMUM_QOS = 0x18,
+  WDS_GPRS_REQUESTED_QOS = 0x19,
+  WDS_GPRS_MINIMUM_QOS = 0x1A,
+  WDS_USERNAME = 0x1B,
+  WDS_PASSWORD = 0x1C,
+  WDS_AUTHENTICATION = 0x1D,
+  WDS_IPV4_ADDRESS_PREFERENCE = 0x1E,
+  WDS_PCSCF_ADDRESS_USING_PCO = 0x1F,
+  WDS_PCSCF_ADDRESS_USING_DHCP = 0x21,
+  WDS_IMCN_FLAG = 0x22,
+  WDS_PDP_CONTEXT_NUMBER = 0x25,
+  WDS_PDP_CONTEXT_SECONDARY_FLAG = 0x26,
+  WDS_PDP_CONTEXT_PRIMARY_ID = 0x27,
+  WDS_IPV6_ADDRESS_PREFERENCE = 0x28,
+  WDS_UMTS_REQUESTED_QOS_WITH_SIGNALING_INDICATION_FLAG = 0x29,
+  WDS_UMTS_MINIMUM_QOS_WITH_SIGNALING_INDICATION_FLAG = 0x2A,
+  WDS_IPV6_PRIMARY_DNS_ADDRESS_PREFERENCE = 0x2B,
+  WDS_IPV6_SECONDARY_DNS_ADDRESS_PREFERENCE = 0x2C,
+  WDS_LTE_QOS_PARAMETERS = 0x2E,
+  WDS_APN_DISABLED_FLAG = 0x2F,
+  WDS_ROAMING_DISALLOWED_FLAG = 0x3E,
+  WDS_APN_TYPE_MASK = 0xDD,
+  WDS_RESET = 0x0000,
+  WDS_SET_EVENT_REPORT = 0x0001,
+  WDS_EVENT_REPORT = 0x0001,
+  WDS_ABORT = 0x0002,
+  WDS_INDICATION_REGISTER = 0x0003,
+  WDS_GET_SUPPORTED_MESSAGES = 0x001E,
+  WDS_START_NETWORK = 0x0020,
+  WDS_STOP_NETWORK = 0x0021,
+  WDS_GET_PACKET_SERVICE_STATUS = 0x0022,
+  WDS_PACKET_SERVICE_STATUS = 0x0022,
+  WDS_GET_CHANNEL_RATES = 0x0023,
+  WDS_GET_PACKET_STATISTICS = 0x0024,
+  WDS_GO_DORMANT = 0x0025,
+  WDS_GO_ACTIVE = 0x0026,
+  WDS_CREATE_PROFILE = 0x0027,
+  WDS_MODIFY_PROFILE = 0x0028,
+  WDS_DELETE_PROFILE = 0x0029,
+  WDS_GET_PROFILE_LIST = 0x002A,
+  WDS_GET_PROFILE_SETTINGS = 0x002B,
+  WDS_GET_DEFAULT_SETTINGS = 0x002C,
+  WDS_GET_CURRENT_SETTINGS = 0x002D,
+  WDS_GET_DORMANCY_STATUS = 0x0030,
+  WDS_GET_AUTOCONNECT_SETTINGS = 0x0034,
+  WDS_GET_DATA_BEARER_TECHNOLOGY = 0x0037,
+  WDS_GET_CURRENT_DATA_BEARER_TECHNOLOGY = 0x0044,
+  WDS_GET_DEFAULT_PROFILE_NUMBER = 0x0049,
+  WDS_SET_DEFAULT_PROFILE_NUMBER = 0x004A,
+  WDS_SET_IP_FAMILY = 0x004D,
+  WDS_SET_AUTOCONNECT_SETTINGS = 0x0051,
+  WDS_GET_PDN_THROTTLE_INFO = 0x006C,
+  WDS_GET_LTE_ATTACH_PARAMETERS = 0x0085,
+  WDS_BIND_DATA_PORT = 0x0089,
+  WDS_EXTENDED_IP_CONFIG = 0x008C,
+  WDS_GET_MAX_LTE_ATTACH_PDN_NUMBER = 0x0092,
+  WDS_SET_LTE_ATTACH_PDN_LIST = 0x0093,
+  WDS_GET_LTE_ATTACH_PDN_LIST = 0x0094,
+  WDS_BIND_MUX_DATA_PORT = 0x00A2,
+  WDS_CONFIGURE_PROFILE_EVENT_LIST = 0x00A7,
+  WDS_PROFILE_CHANGED = 0x00A8,
+  WDS_SWI_CREATE_PROFILE_INDEXED = 0x5558,
 };
 
-struct apn_config{
+static const struct {
+  uint16_t id;
+  const char *cmd;
+} wds_svc_commands[] = {
+    {WDS_EXTENDED_ERROR_CODE, "Extended Error Code"},
+    {WDS_PROFILE_IDENTIFIER, "Profile Identifier"},
+    {WDS_PROFILE_NAME, "Profile Name"},
+    {WDS_PDP_TYPE, "PDP Type"},
+    {WDS_PDP_HEADER_COMPRESSION_TYPE, "PDP Header Compression Type"},
+    {WDS_PDP_DATA_COMPRESSION_TYPE, "PDP Data Compression Type"},
+    {WDS_APN_NAME, "APN Name"},
+    {WDS_PRIMARY_IPV4_DNS_ADDRESS, "Primary IPv4 DNS Address"},
+    {WDS_SECONDARY_IPV4_DNS_ADDRESS, "Secondary IPv4 DNS Address"},
+    {WDS_UMTS_REQUESTED_QOS, "UMTS Requested QoS"},
+    {WDS_UMTS_MINIMUM_QOS, "UMTS Minimum QoS"},
+    {WDS_GPRS_REQUESTED_QOS, "GPRS Requested QoS"},
+    {WDS_GPRS_MINIMUM_QOS, "GPRS Minimum QoS"},
+    {WDS_USERNAME, "Username"},
+    {WDS_PASSWORD, "Password"},
+    {WDS_AUTHENTICATION, "Authentication"},
+    {WDS_IPV4_ADDRESS_PREFERENCE, "IPv4 Address Preference"},
+    {WDS_PCSCF_ADDRESS_USING_PCO, "PCSCF Address Using PCO"},
+    {WDS_PCSCF_ADDRESS_USING_DHCP, "PCSCF Address Using DHCP"},
+    {WDS_IMCN_FLAG, "IMCN Flag"},
+    {WDS_PDP_CONTEXT_NUMBER, "PDP Context Number"},
+    {WDS_PDP_CONTEXT_SECONDARY_FLAG, "PDP Context Secondary Flag"},
+    {WDS_PDP_CONTEXT_PRIMARY_ID, "PDP Context Primary ID"},
+    {WDS_IPV6_ADDRESS_PREFERENCE, "IPv6 Address Preference"},
+    {WDS_UMTS_REQUESTED_QOS_WITH_SIGNALING_INDICATION_FLAG,
+     "UMTS Requested QoS With Signaling Indication Flag"},
+    {WDS_UMTS_MINIMUM_QOS_WITH_SIGNALING_INDICATION_FLAG,
+     "UMTS Minimum QoS With Signaling Indication Flag"},
+    {WDS_IPV6_PRIMARY_DNS_ADDRESS_PREFERENCE,
+     "IPv6 Primary DNS Address Preference"},
+    {WDS_IPV6_SECONDARY_DNS_ADDRESS_PREFERENCE,
+     "IPv6 Secondary DNS Address Preference"},
+    {WDS_LTE_QOS_PARAMETERS, "LTE QoS Parameters"},
+    {WDS_APN_DISABLED_FLAG, "APN Disabled Flag"},
+    {WDS_ROAMING_DISALLOWED_FLAG, "Roaming Disallowed Flag"},
+    {WDS_APN_TYPE_MASK, "APN Type Mask"},
+    {WDS_RESET, "Reset"},
+    {WDS_SET_EVENT_REPORT, "Set Event Report"},
+    {WDS_EVENT_REPORT, "Event Report"},
+    {WDS_ABORT, "Abort"},
+    {WDS_INDICATION_REGISTER, "Indication Register"},
+    {WDS_GET_SUPPORTED_MESSAGES, "Get Supported Messages"},
+    {WDS_START_NETWORK, "Start Network"},
+    {WDS_STOP_NETWORK, "Stop Network"},
+    {WDS_GET_PACKET_SERVICE_STATUS, "Get Packet Service Status"},
+    {WDS_PACKET_SERVICE_STATUS, "Packet Service Status"},
+    {WDS_GET_CHANNEL_RATES, "Get Channel Rates"},
+    {WDS_GET_PACKET_STATISTICS, "Get Packet Statistics"},
+    {WDS_GO_DORMANT, "Go Dormant"},
+    {WDS_GO_ACTIVE, "Go Active"},
+    {WDS_CREATE_PROFILE, "Create Profile"},
+    {WDS_MODIFY_PROFILE, "Modify Profile"},
+    {WDS_DELETE_PROFILE, "Delete Profile"},
+    {WDS_GET_PROFILE_LIST, "Get Profile List"},
+    {WDS_GET_PROFILE_SETTINGS, "Get Profile Settings"},
+    {WDS_GET_DEFAULT_SETTINGS, "Get Default Settings"},
+    {WDS_GET_CURRENT_SETTINGS, "Get Current Settings"},
+    {WDS_GET_DORMANCY_STATUS, "Get Dormancy Status"},
+    {WDS_GET_AUTOCONNECT_SETTINGS, "Get Autoconnect Settings"},
+    {WDS_GET_DATA_BEARER_TECHNOLOGY, "Get Data Bearer Technology"},
+    {WDS_GET_CURRENT_DATA_BEARER_TECHNOLOGY,
+     "Get Current Data Bearer Technology"},
+    {WDS_GET_DEFAULT_PROFILE_NUMBER, "Get Default Profile Number"},
+    {WDS_SET_DEFAULT_PROFILE_NUMBER, "Set Default Profile Number"},
+    {WDS_SET_IP_FAMILY, "Set IP Family"},
+    {WDS_SET_AUTOCONNECT_SETTINGS, "Set Autoconnect Settings"},
+    {WDS_GET_PDN_THROTTLE_INFO, "Get PDN Throttle Info"},
+    {WDS_GET_LTE_ATTACH_PARAMETERS, "Get LTE Attach Parameters"},
+    {WDS_BIND_DATA_PORT, "Bind Data Port"},
+    {WDS_EXTENDED_IP_CONFIG, "Extended Ip Config"},
+    {WDS_GET_MAX_LTE_ATTACH_PDN_NUMBER, "Get Max LTE Attach PDN Number"},
+    {WDS_SET_LTE_ATTACH_PDN_LIST, "Set LTE Attach PDN List"},
+    {WDS_SET_LTE_ATTACH_PDN_LIST, "Set LTE Attach PDN List"},
+    {WDS_GET_LTE_ATTACH_PDN_LIST, "Get LTE Attach PDN List"},
+    {WDS_BIND_MUX_DATA_PORT, "Bind Mux Data Port"},
+    {WDS_CONFIGURE_PROFILE_EVENT_LIST, "Configure Profile Event List"},
+    {WDS_PROFILE_CHANGED, "Profile Changed"},
+    {WDS_SWI_CREATE_PROFILE_INDEXED, "Swi Create Profile Indexed"},
+};
+
+struct apn_config {
   uint8_t type; // 0x14
   uint16_t len; // sizeof
-  char apn[8]; // Null to get default, name
+  char apn[8];  // Null to get default, name
 } __attribute__((packed));
 
-struct apn_type{
-  uint8_t type; // 0x38
-  uint16_t len; // 0x04 (uint32)
+struct apn_type {
+  uint8_t type;   // 0x38
+  uint16_t len;   // 0x04 (uint32)
   uint32_t value; // See APN_TYPE enums
 } __attribute__((packed));
 
-struct ip_family_preference{
-  uint8_t type; // 0x19
-  uint16_t len; // 0x01 (uint8)
+struct ip_family_preference {
+  uint8_t type;  // 0x19
+  uint16_t len;  // 0x01 (uint8)
   uint8_t value; // 4 || 6 || 8 -> IPv4 || IPv6 || Not specified
 } __attribute__((packed));
 
 struct profile_index {
-  uint8_t type; // 0x31
-  uint16_t len; // 0x01 (uint8)
+  uint8_t type;  // 0x31
+  uint16_t len;  // 0x01 (uint8)
   uint8_t value; // I think this is the PDP context ID
 } __attribute__((packed));
 
-struct autoconnect_config{
-  uint8_t type; // 0x33
-  uint16_t len; // 0x01 (uint8)
+struct autoconnect_config {
+  uint8_t type;  // 0x33
+  uint16_t len;  // 0x01 (uint8)
   uint8_t value; // 0 OFF || 1 ON
 } __attribute__((packed));
 
-struct call_type{
-  uint8_t type; // 0x35
-  uint16_t len; // 0x01 (uint8)
+struct call_type {
+  uint8_t type;  // 0x35
+  uint16_t len;  // 0x01 (uint8)
   uint8_t value; // 0 LAPTOP || 1 EMBEDDED
 } __attribute__((packed));
 
-
-struct block_in_roaming{
-  uint8_t type; // 0x39
-  uint16_t len; // 0x01 (uint8)
+struct block_in_roaming {
+  uint8_t type;  // 0x39
+  uint16_t len;  // 0x01 (uint8)
   uint8_t value; // 0 OFF || 1 Don't connect if roaming
 } __attribute__((packed));
 
-struct ep_id{
-  uint8_t type; // 0x10
-  uint16_t len; // 0x08
-  uint32_t ep_type; // peripheral_ep_type in ipc.h
+struct ep_id {
+  uint8_t type;          // 0x10
+  uint16_t len;          // 0x08
+  uint32_t ep_type;      // peripheral_ep_type in ipc.h
   uint32_t interface_id; // ??
 } __attribute__((packed));
 
-struct mux_id{
-  uint8_t type; // 0x11
-  uint16_t len; // 0x01
+struct mux_id {
+  uint8_t type;   // 0x11
+  uint16_t len;   // 0x01
   uint8_t mux_id; // from dpm
 } __attribute__((packed));
 
@@ -103,7 +242,6 @@ struct wds_bind_mux_data_port_request {
   struct ep_id peripheral_id;
   struct mux_id mux;
 } __attribute__((packed));
-
 
 struct wds_start_network {
   struct qmux_packet qmux;
@@ -127,4 +265,7 @@ void reset_wds_runtime();
 uint8_t is_wds_initialized();
 int wds_attempt_to_connect();
 void *init_internal_networking();
+const char *get_wds_command(uint16_t msgid);
+
+int handle_incoming_wds_message(uint8_t *buf, size_t buf_len);
 #endif
--- recipes-modem/openqti/files/src/wds_client.c
+++ recipes-modem/openqti/files/src/wds_client.c
@@ -22,126 +22,27 @@
 #include "../inc/wds.h"
 
 struct {
-  uint8_t service_id;      // 0x01: Wireless Data Service
-  uint8_t instance_id;     // Dynamically allocated
-  uint16_t transaction_id; // Current transaction ID
-  uint8_t is_initialized;
-  uint8_t mux_ready;
-  int internal_smd_fd;
+  uint8_t mux_state;
   int pdp_session_handle; // we need this to stop the connection later
 } wds_runtime;
 
 void reset_wds_runtime() {
-  wds_runtime.service_id = 0x01;
-  wds_runtime.instance_id = 0x00;
-  wds_runtime.transaction_id = 0;
-  wds_runtime.is_initialized = 0;
-  wds_runtime.mux_ready = 0;
-  wds_runtime.internal_smd_fd = -1;
+  wds_runtime.mux_state = 0;
   wds_runtime.pdp_session_handle = -1;
 }
 
-uint8_t is_wds_initialized() { return wds_runtime.is_initialized; }
-
-size_t read_from_port(void *buff, size_t max_sz) {
-  size_t bytes_read = 0;
-  fd_set readfds;
-  struct timeval tv;
-
-  memset(buff, 0, max_sz);
-  tv.tv_sec = 0;
-  tv.tv_usec = 500000;
-  FD_SET(wds_runtime.internal_smd_fd, &readfds);
-  select(MAX_FD, &readfds, NULL, NULL, &tv);
-  if (FD_ISSET(wds_runtime.internal_smd_fd, &readfds)) {
-    logger(MSG_INFO, "%s: Is set!\n", __func__);
-    bytes_read = read(wds_runtime.internal_smd_fd, buff, max_sz);
+const char *get_wds_command(uint16_t msgid) {
+  for (uint16_t i = 0;
+       i < (sizeof(wds_svc_commands) / sizeof(wds_svc_commands[0])); i++) {
+    if (wds_svc_commands[i].id == msgid) {
+      return wds_svc_commands[i].cmd;
+    }
   }
-  set_log_level(0);
-  dump_pkt_raw(buff, bytes_read);
-  set_log_level(1);
-  logger(MSG_INFO, "%s: Bytes read: %u\n", __func__, bytes_read);
-  return bytes_read;
-}
-
-size_t write_to_port(void *buff, size_t bufsz) {
-  size_t bytes_written = 0;
-  set_log_level(0);
-  dump_pkt_raw(buff, bufsz);
-  set_log_level(1);
-  bytes_written = write(wds_runtime.internal_smd_fd, buff, bufsz);
-  if (bytes_written != bufsz) {
-    logger(MSG_WARN,
-           "%s: Heey we wrote %u bytes from %u that we were supposed to...\n",
-           __func__, bytes_written, bufsz);
-  }
-  return bytes_written;
-}
-
-int allocate_internal_wds_client() {
-  uint8_t *buff = malloc(1024 * sizeof(uint8_t));
-  struct client_alloc_request *request = NULL;
-  size_t bytes = 0;
-  int ret = 0;
-  wds_runtime.internal_smd_fd = open(INT_SMD_CNTL, O_RDWR);
-  if (wds_runtime.internal_smd_fd < 0) {
-    logger(MSG_ERROR, "Error opening %s!\n", INT_SMD_CNTL);
-    return -EINVAL;
-  }
-
-  request = (struct client_alloc_request *)buff;
-  request->qmux.version = 0x01;
-  request->qmux.packet_length = 0x0f;
-  request->qmux.service = 0x00;
-  request->qmux.instance_id = 0x00;
-
-  request->qmi.id = 0x06;
-  request->qmi.msgid = 0x0022;
-  request->qmi.size = 0x04;
-
-  request->service.id = 0x01;
-  request->service.len = 0x01;
-  request->service.service_id = 0x01; // 0x01 -> WDS
-  if (write_to_port((void *)request, sizeof(struct client_alloc_request)) <
-      sizeof(struct client_alloc_request)) {
-    logger(MSG_ERROR, "Allocation failed\n");
-    free(buff);
-    return -ENOMEM;
-  }
-  memset(buff, 0, 1024);
-  bytes = read_from_port(buff, 1024);
-  if (bytes >= sizeof(struct client_alloc_response)) {
-    logger(MSG_INFO, "We may have a valid response, let's check it\n");
-    struct client_alloc_response *response =
-        (struct client_alloc_response *)buff;
-      if (response->result.response == 0x00 &&
-          response->result.result == 0x00) {
-        logger(MSG_INFO, "Yesss we allocated or client, instance ID: %.2x\n",
-               response->instance.instance_id);
-        wds_runtime.service_id = response->instance.service_id;
-        wds_runtime.instance_id = response->instance.instance_id;
-        wds_runtime.transaction_id = response->qmi.transaction_id;
-        wds_runtime.is_initialized = 1;
-      }
-    /*
-      If we have a 0x02 and a 0x01, and sizes are right, check indication result
-    */
-    //  int offset = get_tlv_offset_by_id(buff, bytes, 0x02);
-    //  logger(MSG_INFO, "TLV 0x02 offset at %.2x\n", offset);
-  } else {
-    logger(MSG_ERROR, "%s: Invalid allocation response: %u bytes\n", __func__,
-           bytes);
-  }
-  free(buff);
-  return ret;
+  return "WDS service: Unknown command\n";
 }
 
 int wds_bind_mux_data_port() {
   struct wds_bind_mux_data_port_request *request = NULL;
-  int ret = 0;
-  uint16_t qmi_err = 0;
-  size_t bytes = 0;
-  uint8_t *buff = malloc(1024 * sizeof(uint8_t));
 
   if (is_internal_connect_enabled()) {
     logger(MSG_INFO, "%s: Bind mux data port request!\n", __func__);
@@ -150,18 +51,18 @@ int wds_bind_mux_data_port() {
            __func__);
     return -EINVAL;
   }
-  wds_runtime.transaction_id++;
   request = (struct wds_bind_mux_data_port_request *)calloc(
       1, sizeof(struct wds_bind_mux_data_port_request));
+
   request->qmux.version = 0x01;
   request->qmux.packet_length =
       sizeof(struct wds_bind_mux_data_port_request) - sizeof(uint8_t);
   request->qmux.control = 0x00;
-  request->qmux.service = wds_runtime.service_id;
-  request->qmux.instance_id = wds_runtime.instance_id;
+  request->qmux.service = QMI_SERVICE_WDS;
+  //  request->qmux.instance_id = wds_runtime.instance_id;
 
-  request->qmi.ctlid = 0x00;
-  request->qmi.transaction_id = wds_runtime.transaction_id;
+  request->qmi.ctlid = QMI_REQUEST;
+  //  request->qmi.transaction_id = wds_runtime.transaction_id;
   request->qmi.msgid = WDS_BIND_MUX_DATA_PORT;
   request->qmi.length = sizeof(struct wds_bind_mux_data_port_request) -
                         sizeof(struct qmux_packet) - sizeof(struct qmi_packet);
@@ -175,31 +76,14 @@ int wds_bind_mux_data_port() {
   request->mux.len = 0x01;
   request->mux.mux_id = 42;
 
-  bytes = write_to_port(request, sizeof(struct wds_bind_mux_data_port_request));
-  logger(MSG_WARN, "%s: Bytes written %u\n", __func__, bytes);
-  bytes = read_from_port(buff, 1024);
-  logger(MSG_WARN, "%s: Bytes read from buff: %u\n", __func__, bytes);
-  qmi_err = did_qmi_op_fail(buff, bytes);
-  if (qmi_err == QMI_RESULT_FAILURE) {
-    logger(MSG_ERROR, "%s failed\n", __func__);
-    ret = -EFAULT;
-  } else  if (qmi_err == QMI_RESULT_SUCCESS) {
-    logger(MSG_INFO, "%s succeeded\n", __func__);
-    wds_runtime.mux_ready = 1;
-  } else if (qmi_err == QMI_RESULT_UNKNOWN) {
-    logger(MSG_ERROR, "%s: QMI message didn't have an indication\n", __func__);
-    ret = -EINVAL;
-  }
-
+  add_pending_message(QMI_SERVICE_WDS, (uint8_t *)request,
+                              sizeof(struct wds_bind_mux_data_port_request));
   free(request);
-  free(buff);
-  return ret;
+  return 0;
 }
 
 int wds_attempt_to_connect() {
   struct wds_start_network *request = NULL;
-  size_t bytes = 0;
-  uint8_t *buff = malloc(1024 * sizeof(uint8_t));
   if (is_internal_connect_enabled()) {
     logger(MSG_INFO, "%s: Autoconnect request!\n", __func__);
   } else {
@@ -207,30 +91,32 @@ int wds_attempt_to_connect() {
            __func__);
     return -EINVAL;
   }
-  if (!wds_runtime.mux_ready)
+  if (wds_runtime.mux_state == 0) {
     wds_bind_mux_data_port();
-  else
-   logger(MSG_INFO, "Mux was already setup\n");
+  } else if (wds_runtime.mux_state == 1) {
+    logger(MSG_INFO, "%s: Waiting for mux ready...\n", __func__);
+  } else {
+    logger(MSG_INFO, "Mux was already setup\n");
+  }
 
-  wds_runtime.transaction_id++;
   request =
       (struct wds_start_network *)calloc(1, sizeof(struct wds_start_network));
   request->qmux.version = 0x01;
   request->qmux.packet_length =
       sizeof(struct wds_start_network) - sizeof(uint8_t);
   request->qmux.control = 0x00;
-  request->qmux.service = wds_runtime.service_id;
-  request->qmux.instance_id = wds_runtime.instance_id;
+  request->qmux.service = QMI_SERVICE_WDS;
+  //  request->qmux.instance_id = wds_runtime.instance_id;
 
-  request->qmi.ctlid = 0x00;
-  request->qmi.transaction_id = wds_runtime.transaction_id;
-  request->qmi.msgid = WDS_START_NETWORK_IF;
+  request->qmi.ctlid = QMI_REQUEST;
+  //  request->qmi.transaction_id = wds_runtime.transaction_id;
+  request->qmi.msgid = WDS_START_NETWORK;
   request->qmi.length = sizeof(struct wds_start_network) -
                         sizeof(struct qmux_packet) - sizeof(struct qmi_packet);
 
   request->apn.type = 0x14;
   request->apn.len = 8;
-  strcpy(request->apn.apn, DEFAULT_APN_NAME);
+  memcpy(request->apn.apn, DEFAULT_APN_NAME, strlen(DEFAULT_APN_NAME));
 
   request->apn_type.type = 0x38;
   request->apn_type.len = 0x04;
@@ -256,65 +142,10 @@ int wds_attempt_to_connect() {
   request->roaming_lock.len = 0x01;
   request->roaming_lock.value = 0x00; // OFF
 
-  bytes = write_to_port(request, sizeof(struct wds_start_network));
-  logger(MSG_WARN, "%s: Bytes written %u\n", __func__, bytes);
-  bytes = read_from_port(buff, 1024);
-  logger(MSG_WARN, "%s: Bytes read from buff: %u\n", __func__, bytes);
-  if (did_qmi_op_fail(buff, bytes)) {
-    logger(MSG_ERROR, "%s failed\n", __func__);
-  }
+  add_pending_message(QMI_SERVICE_WDS, (uint8_t *)request,
+                              sizeof(struct wds_start_network));
+
   free(request);
-  free(buff);
-  return 0;
-}
-
-
-int wds_attempt_to_connect_slim() {
-  struct wds_start_network_autoconnect *request = NULL;
-  size_t bytes = 0;
-  uint8_t *buff = malloc(1024 * sizeof(uint8_t));
-  if (is_internal_connect_enabled()) {
-    logger(MSG_INFO, "%s: Autoconnect request!\n", __func__);
-  } else {
-    logger(MSG_INFO, "%s: Internal connectivity support is not enabled\n",
-           __func__);
-    return -EINVAL;
-  }
-  if (!wds_runtime.mux_ready)
-    wds_bind_mux_data_port();
-  else
-   logger(MSG_INFO, "Mux was already setup\n");
-
-  wds_runtime.transaction_id++;
-  request =
-      (struct wds_start_network_autoconnect *)calloc(1, sizeof(struct wds_start_network));
-  request->qmux.version = 0x01;
-  request->qmux.packet_length =
-      sizeof(struct wds_start_network_autoconnect) - sizeof(uint8_t);
-  request->qmux.control = 0x00;
-  request->qmux.service = wds_runtime.service_id;
-  request->qmux.instance_id = wds_runtime.instance_id;
-
-  request->qmi.ctlid = 0x00;
-  request->qmi.transaction_id = wds_runtime.transaction_id;
-  request->qmi.msgid = WDS_START_NETWORK_IF;
-  request->qmi.length = sizeof(struct wds_start_network_autoconnect) -
-                        sizeof(struct qmux_packet) - sizeof(struct qmi_packet);
-
-  request->autoconnect.type = 0x33;
-  request->autoconnect.len = 0x01;
-  request->autoconnect.value = 0x01; // ON
-
-
-  bytes = write_to_port(request, sizeof(struct wds_start_network_autoconnect));
-  logger(MSG_WARN, "%s: Bytes written %u\n", __func__, bytes);
-  bytes = read_from_port(buff, 1024);
-  logger(MSG_WARN, "%s: Bytes read from buff: %u\n", __func__, bytes);
-  if (did_qmi_op_fail(buff, bytes)) {
-    logger(MSG_ERROR, "%s failed\n", __func__);
-  }
-  free(request);
-  free(buff);
   return 0;
 }
 
@@ -329,29 +160,45 @@ void *init_internal_networking() {
     sleep(10);
   }
 
-  logger(MSG_INFO, "%s: Network is ready!\n", __func__);
-  if (!wds_runtime.is_initialized) {
-    logger(MSG_INFO,
-           "[%s] Attempting to initialize a WDS client...\n", __func__);
-    allocate_internal_wds_client();
-  } else {
-    logger(MSG_INFO, "[%s] Client already initialized, ID %.2x\n",
-           __func__, wds_runtime.instance_id);
-  }
+  logger(MSG_INFO, "%s: Network is ready, try to connect\n", __func__);
+  wds_attempt_to_connect();
 
-  if (wds_runtime.is_initialized) {
-    logger(MSG_INFO, "[%s] Client ready, attempting to connect!\n",
-           __func__);
-    wds_attempt_to_connect();
-  }
-
-
-  if (wds_runtime.is_initialized) {
-    logger(MSG_INFO, "[%s] ATTEMPT 2 attempting to connect!\n",
-           __func__);
-    wds_attempt_to_connect_slim();
-  }
-
-  
   return NULL;
+}
+
+/*
+ * Reroutes messages from the internal QMI client to the service
+ */
+int handle_incoming_wds_message(uint8_t *buf, size_t buf_len) {
+  logger(MSG_INFO, "%s: Start\n", __func__);
+  uint16_t qmi_err;
+
+  switch (get_qmi_message_id(buf, buf_len)) {
+  case WDS_BIND_MUX_DATA_PORT:
+    if (get_qmi_message_type(buf, buf_len) == QMI_RESPONSE) {
+      qmi_err = did_qmi_op_fail(buf, buf_len);
+      if (qmi_err == QMI_RESULT_FAILURE) {
+        logger(MSG_ERROR, "%s failed\n", __func__);
+        wds_runtime.mux_state = 0;
+      } else if (qmi_err == QMI_RESULT_SUCCESS) {
+        logger(MSG_INFO, "%s succeeded\n", __func__);
+        wds_runtime.mux_state = 2;
+      } else if (qmi_err == QMI_RESULT_UNKNOWN) {
+        logger(MSG_ERROR, "%s: QMI message didn't have an indication\n",
+               __func__);
+      }
+    }
+    break;
+  case WDS_START_NETWORK:
+    if (did_qmi_op_fail(buf, buf_len)) {
+      logger(MSG_ERROR, "%s failed\n", __func__);
+    }
+    break;
+  default:
+    logger(MSG_INFO, "%s: Unhandled message for WDS: %.4x\n", __func__,
+           get_qmi_message_id(buf, buf_len));
+    break;
+  }
+
+  return 0;
 }
