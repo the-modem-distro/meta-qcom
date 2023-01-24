6cd19e8dd3311236e61f70bde352cc799a530064
Biktor <biktor@biktorgj.com>
2023-01-24 08:22:53 +0100

    [wds] Add empty switch statements for debugging

--- recipes-modem/openqti/files/src/wds_client.c
+++ recipes-modem/openqti/files/src/wds_client.c
@@ -77,7 +77,7 @@ int wds_bind_mux_data_port() {
   request->mux.mux_id = 42;
 
   add_pending_message(QMI_SERVICE_WDS, (uint8_t *)request,
-                              sizeof(struct wds_bind_mux_data_port_request));
+                      sizeof(struct wds_bind_mux_data_port_request));
   free(request);
   return 0;
 }
@@ -143,7 +143,7 @@ int wds_attempt_to_connect() {
   request->roaming_lock.value = 0x00; // OFF
 
   add_pending_message(QMI_SERVICE_WDS, (uint8_t *)request,
-                              sizeof(struct wds_start_network));
+                      sizeof(struct wds_start_network));
 
   free(request);
   return 0;
@@ -193,6 +193,168 @@ int handle_incoming_wds_message(uint8_t *buf, size_t buf_len) {
     if (did_qmi_op_fail(buf, buf_len)) {
       logger(MSG_ERROR, "%s failed\n", __func__);
     }
+    break;
+  case WDS_EXTENDED_ERROR_CODE:
+    logger(MSG_INFO, "%s: Extended Error Code\n", __func__);
+    break;
+  case WDS_PROFILE_IDENTIFIER:
+    logger(MSG_INFO, "%s: Profile Identifier\n", __func__);
+    break;
+  case WDS_PROFILE_NAME:
+    logger(MSG_INFO, "%s: Profile Name\n", __func__);
+    break;
+  case WDS_PDP_TYPE:
+    logger(MSG_INFO, "%s: PDP Type\n", __func__);
+    break;
+  case WDS_PDP_HEADER_COMPRESSION_TYPE:
+    logger(MSG_INFO, "%s: PDP Header Compression Type\n", __func__);
+    break;
+  case WDS_PDP_DATA_COMPRESSION_TYPE:
+    logger(MSG_INFO, "%s: PDP Data Compression Type\n", __func__);
+    break;
+  case WDS_APN_NAME:
+    logger(MSG_INFO, "%s: APN Name\n", __func__);
+    break;
+  case WDS_PRIMARY_IPV4_DNS_ADDRESS:
+    logger(MSG_INFO, "%s: Primary IPv4 DNS Address\n", __func__);
+    break;
+  case WDS_SECONDARY_IPV4_DNS_ADDRESS:
+    logger(MSG_INFO, "%s: Secondary IPv4 DNS Address\n", __func__);
+    break;
+  case WDS_UMTS_REQUESTED_QOS:
+    logger(MSG_INFO, "%s: UMTS Requested QoS\n", __func__);
+    break;
+  case WDS_UMTS_MINIMUM_QOS:
+    logger(MSG_INFO, "%s: UMTS Minimum QoS\n", __func__);
+    break;
+  case WDS_GPRS_REQUESTED_QOS:
+    logger(MSG_INFO, "%s: GPRS Requested QoS\n", __func__);
+    break;
+  case WDS_GPRS_MINIMUM_QOS:
+    logger(MSG_INFO, "%s: GPRS Minimum QoS\n", __func__);
+    break;
+  case WDS_USERNAME:
+    logger(MSG_INFO, "%s: Username\n", __func__);
+    break;
+  case WDS_PASSWORD:
+    logger(MSG_INFO, "%s: Password\n", __func__);
+    break;
+  case WDS_AUTHENTICATION:
+    logger(MSG_INFO, "%s: Authentication\n", __func__);
+    break;
+  case WDS_LTE_QOS_PARAMETERS:
+    logger(MSG_INFO, "%s: LTE QoS Parameters\n", __func__);
+    break;
+  case WDS_APN_DISABLED_FLAG:
+    logger(MSG_INFO, "%s: APN Disabled Flag\n", __func__);
+    break;
+  case WDS_ROAMING_DISALLOWED_FLAG:
+    logger(MSG_INFO, "%s: Roaming Disallowed Flag\n", __func__);
+    break;
+  case WDS_APN_TYPE_MASK:
+    logger(MSG_INFO, "%s: APN Type Mask\n", __func__);
+    break;
+  case WDS_RESET:
+    logger(MSG_INFO, "%s: Reset\n", __func__);
+    break;
+  case WDS_ABORT:
+    logger(MSG_INFO, "%s: Abort\n", __func__);
+    break;
+  case WDS_INDICATION_REGISTER:
+    logger(MSG_INFO, "%s: Indication Register\n", __func__);
+    break;
+  case WDS_STOP_NETWORK:
+    logger(MSG_INFO, "%s: Stop Network\n", __func__);
+    break;
+  case WDS_GET_PACKET_SERVICE_STATUS:
+    logger(MSG_INFO, "%s: Get Packet Service Status\n", __func__);
+    break;
+  case WDS_GET_CHANNEL_RATES:
+    logger(MSG_INFO, "%s: Get Channel Rates\n", __func__);
+    break;
+  case WDS_GET_PACKET_STATISTICS:
+    logger(MSG_INFO, "%s: Get Packet Statistics\n", __func__);
+    break;
+  case WDS_GO_DORMANT:
+    logger(MSG_INFO, "%s: Go Dormant\n", __func__);
+    break;
+  case WDS_GO_ACTIVE:
+    logger(MSG_INFO, "%s: Go Active\n", __func__);
+    break;
+  case WDS_CREATE_PROFILE:
+    logger(MSG_INFO, "%s: Create Profile\n", __func__);
+    break;
+  case WDS_MODIFY_PROFILE:
+    logger(MSG_INFO, "%s: Modify Profile\n", __func__);
+    break;
+  case WDS_DELETE_PROFILE:
+    logger(MSG_INFO, "%s: Delete Profile\n", __func__);
+    break;
+  case WDS_GET_PROFILE_LIST:
+    logger(MSG_INFO, "%s: Get Profile List\n", __func__);
+    break;
+  case WDS_GET_PROFILE_SETTINGS:
+    logger(MSG_INFO, "%s: Get Profile Settings\n", __func__);
+    break;
+  case WDS_GET_DEFAULT_SETTINGS:
+    logger(MSG_INFO, "%s: Get Default Settings\n", __func__);
+    break;
+  case WDS_GET_CURRENT_SETTINGS:
+    logger(MSG_INFO, "%s: Get Current Settings\n", __func__);
+    break;
+  case WDS_GET_DORMANCY_STATUS:
+    logger(MSG_INFO, "%s: Get Dormancy Status\n", __func__);
+    break;
+  case WDS_GET_AUTOCONNECT_SETTINGS:
+    logger(MSG_INFO, "%s: Get Autoconnect Settings\n", __func__);
+    break;
+  case WDS_GET_DATA_BEARER_TECHNOLOGY:
+    logger(MSG_INFO, "%s: Get Data Bearer Technology\n", __func__);
+    break;
+  case WDS_GET_CURRENT_DATA_BEARER_TECHNOLOGY:
+    logger(MSG_INFO, "%s: Get Current Data Bearer Technology\n", __func__);
+    break;
+  case WDS_GET_DEFAULT_PROFILE_NUMBER:
+    logger(MSG_INFO, "%s: Get Default Profile Number\n", __func__);
+    break;
+  case WDS_SET_DEFAULT_PROFILE_NUMBER:
+    logger(MSG_INFO, "%s: Set Default Profile Number\n", __func__);
+    break;
+  case WDS_SET_IP_FAMILY:
+    logger(MSG_INFO, "%s: Set IP Family\n", __func__);
+    break;
+  case WDS_SET_AUTOCONNECT_SETTINGS:
+    logger(MSG_INFO, "%s: Set Autoconnect Settings\n", __func__);
+    break;
+  case WDS_GET_PDN_THROTTLE_INFO:
+    logger(MSG_INFO, "%s: Get PDN Throttle Info\n", __func__);
+    break;
+  case WDS_GET_LTE_ATTACH_PARAMETERS:
+    logger(MSG_INFO, "%s: Get LTE Attach Parameters\n", __func__);
+    break;
+  case WDS_BIND_DATA_PORT:
+    logger(MSG_INFO, "%s: Bind Data Port\n", __func__);
+    break;
+  case WDS_EXTENDED_IP_CONFIG:
+    logger(MSG_INFO, "%s: Extended Ip Config\n", __func__);
+    break;
+  case WDS_GET_MAX_LTE_ATTACH_PDN_NUMBER:
+    logger(MSG_INFO, "%s: Get Max LTE Attach PDN Number\n", __func__);
+    break;
+  case WDS_SET_LTE_ATTACH_PDN_LIST:
+    logger(MSG_INFO, "%s: Set LTE Attach PDN List\n", __func__);
+    break;
+  case WDS_GET_LTE_ATTACH_PDN_LIST:
+    logger(MSG_INFO, "%s: Get LTE Attach PDN List\n", __func__);
+    break;
+  case WDS_CONFIGURE_PROFILE_EVENT_LIST:
+    logger(MSG_INFO, "%s: Configure Profile Event List\n", __func__);
+    break;
+  case WDS_PROFILE_CHANGED:
+    logger(MSG_INFO, "%s: Profile Changed\n", __func__);
+    break;
+  case WDS_SWI_CREATE_PROFILE_INDEXED:
+    logger(MSG_INFO, "%s: Swi Create Profile Indexed\n", __func__);
     break;
   default:
     logger(MSG_INFO, "%s: Unhandled message for WDS: %.4x\n", __func__,
